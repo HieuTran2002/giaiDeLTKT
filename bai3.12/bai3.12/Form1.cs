391c8cd50d9311117f61975a3075e0ef2aa63fc1
codeflac <hieudubstep2001@gmail.com>
2022-06-07 10:09:02 +0700

    update 3.12

---
+++ bai3.12/bai3.12/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai3._12
+{
+    public partial class Form1 : Form
+    {
+        double[] ring5Value = { 0, 1 , 2 , 0.05, 0.25 , 0.5, 5, 10, 20};
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+            comboBox5.SelectedIndex = 0;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = System.Drawing.ColorTranslator.FromHtml(comboBox1.Text);
+            calculate();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = System.Drawing.ColorTranslator.FromHtml(comboBox2.Text);
+            calculate();
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox3.BackColor = System.Drawing.ColorTranslator.FromHtml(comboBox3.Text);
+            calculate();
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox4.BackColor = System.Drawing.ColorTranslator.FromHtml(comboBox4.Text);
+            calculate();
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox5.SelectedIndex == 0)
+            {
+                pictureBox5.BackColor = this.BackColor;
+            }
+            else
+            {
+                pictureBox5.BackColor = System.Drawing.ColorTranslator.FromHtml(comboBox5.Text);
+
+            }
+            calculate();
+        }
+
+        private void calculate()
+        {
+            double ring1 = comboBox1.SelectedIndex;
+            double ring2 = comboBox2.SelectedIndex;
+            double ring3 = comboBox3.SelectedIndex;
+            double ring4 = 0;
+            double ring5;
+            double result1 = 0;
+
+            //Kiểm tra giá trị cho vòng 5
+            if (comboBox5.SelectedIndex < 0)
+            {
+                ring5 = 0;
+            }
+            else
+            {
+                ring5 = ring5Value[comboBox5.SelectedIndex];
+            }
+
+            //Gán giá trị cho vòng 4
+            if (comboBox4.Text == "gold")
+            {
+                ring4 = -1;
+            }
+            else if(comboBox4.Text == "silver")
+            {
+                ring4 = -2;
+            }
+            else
+            {
+                ring4 = comboBox4.SelectedIndex + 1;
+
+            }
+
+            //tính giá trị 4 vòng đầu
+            if (ring1 != 0)
+            {
+                result1 = (ring1 * 100 + ring2 * 10 + ring3)*(Math.Pow(10, ring4));
+            }
+            else
+            {
+                result1 = 0;
+            }
+
+            label1.Text = ring1.ToString();
+            label2.Text = ring2.ToString();
+            label3.Text = ring3.ToString();
+            label4.Text = ring4.ToString();
+            label5.Text = "+- " + ring5.ToString() + "%";
+            label6.Text = result1.ToString() + "    " + label5.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ bai3.4/bai3.4/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai3._4
+{
+    public partial class Form1 : Form
+    {
+        int[,] myArray = new int[5, 5];
+        int col = 0;
+        int row = 0;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt32(textBox1.Text);
+            string output = "";
+            if (count < 25)
+            {
+
+                myArray[row, col] = input;
+                count++;
+                col++;
+
+                if (count % 5 == 0)
+                {
+                    row++;
+                    col = 0;
+
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    output += myArray[i, j].ToString() + "  ";
+                }
+                output += "\n";
+            }
+
+            label2.Text = getArray();
+
+        }
+        public string getArray()
+        {
+            string output = "";
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    output += myArray[i, j].ToString() + "  ";
+                }
+                output += "\n";
+            }
+
+            return output;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myArray = new int[5, 5];
+            col = 0;
+            row = 0;
+            count = 0;
+
+            label2.Text = getArray();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int result1 = 0;
+            int result2 = 0;
+            int result3 = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                result1 += myArray[i, i] + myArray[i, 4 - i];
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((i == 0 || i == 4) || (j == 0 || j == 4))
+                    {
+                        result2 += myArray[i, j];
+
+                    }
+                    result3 += myArray[i, j];
+                }
+            }
+            result1 -= myArray[2,2];
+
+            label3.Text = result1.ToString();
+            label6.Text = result2.ToString();
+            label8.Text = result3.ToString();
+        }
+    }
+}
